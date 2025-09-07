d2056bc067ba2a72206aca508a28bfe79ae40828
Aditya Mathur <adi.mathur89@gmail.com>
2025-09-07 14:59:47 +0530

    change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello on AWS App Runner!" });
+  res.json({ msg: "Hello world on AWS App Runner!" });
 });
 
 app.get("/health", (req, res) => res.status(200).send("OK"));
